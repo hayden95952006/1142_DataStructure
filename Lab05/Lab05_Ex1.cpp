f7156740c1209b5c6d00c1afe8a4f8e1ef1610e9
hayden95952006 <hayden95952006@gmail.com>
2026-04-01 20:15:47 +0800

    Create Lab05_Ex1.cpp

---
+++ Lab05/Lab05_Ex1.cpp
@@ -0,0 +1,39 @@
+#include<iostream>
+#include<string>
+usingnamespacestd;
+
+//建立一個名為Student的結構型態,裡面包含int、string等不同類型的資料型態
+structStudent{
+  intid;// 學生ID
+	stringname;// 學生姓名
+	intchinese;// 國文分數
+	intenglish;// 英文分數
+	intmath;// 數學分數
+	intsocial;// 社會分數
+	intscience;// 自然分數
+};
+// 使用call by address的方式回傳值
+voidupdateMath(Student*s){		
+	s->math=100;//透過->改變結構成員的數值
+}
+// 使用call by value的方式回傳值
+voidprintStudent(Students){		
+	cout<<"學生編號: "<<s.id<<endl;
+	cout<<"姓名: "<<s.name<<endl;
+	cout<<"數學成績: "<<s.math<<endl;
+}
+intmain(){
+	Studentone;
+	// 利用前面所建立的名為Studunt的結構來宣告一個名為 one 的結構變數
+	// 對新宣告的結構變數 one 中的元素賦值
+	one.id=1;// 設定學生ID
+	one.name="張小美";// 設定姓名
+	one.chinese=77;// 設定國文分數
+	one.english=83;// 設定英文分數
+	one.math=86;// 設定數學分數
+	one.social=68;// 設定社會分數
+	one.science=91;// 設定自然分數
+	updateMath(&one);// 利用指標 s 指向 one 的位址，才能使函式直接修改原本數值				結構中的資料。
+	printStudent(one);// 把one的資料複製一份給函式s做輸出
+	return0;
+}
